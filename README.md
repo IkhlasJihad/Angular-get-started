a0a065c06f975b23da0bef62c9dffe79330be251
IkhlasJihad <ikhlasjaydi@gmail.com>
2021-07-10 00:30:12 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-clmz5d
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-clmz5d)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = products;
+  share(name: string) {
+    window.alert(`${name} product has been shared!`);
+  }
+  onNotify(name: string) {
+    window.alert(`You'll be notified when ${name} product goes on sale!`);
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
